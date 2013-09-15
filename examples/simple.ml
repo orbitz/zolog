de52de60a3af0d8ae831afecba56b16ab0c91de4
orbitz <orbitz@gmail.com>
2013-09-15 15:14:48 +0200

    Add some comments

--- examples/simple.ml
+++ examples/simple.ml
@@ -4,34 +4,46 @@ open Async.Std
 let main () =
   let open Deferred.Monad_infix in
   let logger = Zolog.start () in
+
+  (* Create a default formatter *)
   let formatter =
     Zolog_std_event_writer_backend.default_formatter
-      ~max_level:Zolog_std_event.Log.Critical
   in
+
+  (*
+   * Create a backend and funnel all messages to Debug
+   * and use the stderr rotator, which doesn't rotate
+   * anything and it only createsa debug level writer
+   *)
   Zolog_std_event_writer_backend.create
     (Zolog_std_event_writer_backend.funnel formatter)
     Zolog_std_event_writer_backend.stderr_rotator
   >>= fun backend ->
+
   let handler = Zolog_std_event_writer_backend.handler backend in
   Zolog.add_handler logger handler >>= fun id ->
+  (* Log a debug statement *)
   Zolog_event.debug
     ~n:["example"; "name"]
     ~o:"simple"
     logger
     "debug message"
   >>= fun () ->
+  (* Log a critical *)
   Zolog_event.critical
     ~n:["example"; "name"; "critical"]
     ~o:"simple"
     logger
     "critical message"
   >>= fun () ->
+  (* Count something *)
   Zolog_event.counter
     ~n:["example"; "name"; "counter"]
     ~o:"simple"
     logger
     5
   >>= fun () ->
+  (* Time something *)
   Zolog_event.time
     ~n:["example"; "name"; "timer"]
     ~o:"simple"
